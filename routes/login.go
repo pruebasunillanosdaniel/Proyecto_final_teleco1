cb5ffd77f1923472422451e0c8044fed2cbb211b
pruebasunillanosdaniel <daniel.salgado@unillanos.edu.co>
2023-07-05 17:09:43 -0500

    secrets and fiber routes with dinamic port12

--- routes/login.go
+++ routes/login.go
@@ -20,12 +20,7 @@ func Valida_db(c *fiber.Ctx) error {
 }
 
 func Valida_db_variables(c *fiber.Ctx) error {
-	db, err := database.Database()
 
-	if err != nil {
-		return c.JSON(fiber.Map{"host": utilidades.Azure_host, "port": utilidades.Azure_port})
-
-	}
-	return c.JSON(fiber.Map{"error": "OK" + fmt.Sprint(db)})
+	return c.JSON(fiber.Map{"host": utilidades.Azure_host, "port": utilidades.Azure_port})
 
 }
