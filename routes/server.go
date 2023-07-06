42cd1f60c5e09fb0dcce894e0c8fdbb23b183049
pruebasunillanosdaniel <daniel.salgado@unillanos.edu.co>
2023-07-06 01:59:52 -0500

    cambios a docker container 8

--- routes/server.go
+++ routes/server.go
@@ -15,17 +15,16 @@ func Crear_servidor() {
 		return c.JSON(map[string]string{"aaa": "asdasdf"})
 	})
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Get("", func(c *fiber.Ctx) error {
 		return c.JSON(map[string]string{"aaa": "asdasdf"})
 	})
-
 	app.Get("/List_users", List_all_usuario)
-	app.Post("/Create", Create_usuario)
-	app.Get("/READ", Get_usuario)
-	app.Patch("/UPDATE", Update_usuario)
-	app.Delete("/List_users", Delete_usuario)
-	app.Get("/Validar", Valida_db)
-	app.Get("/PATHS", Valida_db_variables)
+	app.Post("/crearusuario", Create_usuario)
+	app.Get("/leer_usaurio", Get_usuario)
+	app.Patch("/actualizarusuario", Update_usuario)
+	app.Delete("/eliminar_usuario", Delete_usuario)
+	app.Get("/validar", Valida_db)
+	app.Get("/paths", Valida_db_variables)
 
 	port := os.Getenv("HTTP_PLATFORM_PORT")
 
@@ -34,6 +33,6 @@ func Crear_servidor() {
 		port = "80"
 	}
 
-	app.Listen(":" + "80")
+	app.Listen(":" + "9000")
 
 }
